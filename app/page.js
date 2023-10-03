9fe0be7629085a0687ab07cace9cdfd971a20f9d
profabhishekjha <jhaabhishek910@gmail.com>
2023-10-03 12:04:55 +0530

    base layer

--- app/page.js
+++ app/page.js
@@ -1,34 +1,8 @@
-"use client";
+import React from "react";
+import Layout from "./components/Layout";
 
-import { signIn, signOut, useSession } from "next-auth/react";
+const Home = () => {
+  return <Layout>Test</Layout>;
+};
 
-export default function Home() {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-400 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2 rounded-lg"
-          >
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-  return (
-    <>
-      <div className="bg-blue-400 flex items-center justify-center">
-        Logged in as '{session.user.email}'
-      </div>
-      <button
-        onClick={() => signOut("google")}
-        className="bg-white p-2 rounded-lg"
-      >
-        Logout
-      </button>
-    </>
-  );
-}
+export default Home;
